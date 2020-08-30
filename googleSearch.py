f4846160bebafa0b63b1e6da283c4d5b059eb253
Hazem <hazeemahmed22@gmail.com>
2020-08-30 23:32:23 +0200

    new

--- googleSearch.py
+++ googleSearch.py
@@ -1,30 +1,15 @@
-import webbrowser
 import json
 from urllib.request import urlopen
 from flask import render_template
 
 
 try:
-    def search(word):
-        url = "https://www.google.com.tr/search?q={}".format(word)
-        return webbrowser.get().open_new_tab(url)
-
-    def location(place):
-        url = "https://www.google.nl/maps/place/" + place + "/&amp;"
-        return webbrowser.get().open_new_tab(url)
-
-    def myip():
-        url = "http://ipinfo.io/json"
-        return webbrowser.get().open_new_tab(url)
-
     def mylocation():
         url = "http://ipinfo.io/json"
         response = urlopen(url)
         data = json.load(response)
         location = data['loc']
-        print(location)
-        url = "https://www.google.nl/maps/place/" + location + "/&amp;"
-        return webbrowser.get().open_new_tab(url)
+        return location
 
 except Exception as ex:
     print(ex)
