7d6e7f0a26474f5fbde322c7d0e87526b74f48fc
unknown <hazeemahmed22@gmail.com>
2020-12-12 22:35:40 +0200

    404 page & updates

--- app.py
+++ app.py
@@ -12,9 +12,18 @@ bot = RiveScript()
 bot.load_directory(brain)
 bot.sort_replies()
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html')
+
 @app.route('/')
 def callchat():
-    return render_template('chat.html')
+    return render_template('index.html')
+
+@app.route('/homepage')
+def callHome():
+    return render_template('index.html')
 
 @app.route("/get")
 def get_bot_response():
