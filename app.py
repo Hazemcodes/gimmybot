1ebad7f714b15e30863d257e44bf7eff6ee9455d
unknown <hazeemahmed22@gmail.com>
2020-12-12 23:23:34 +0200

    updates

--- app.py
+++ app.py
@@ -30,6 +30,3 @@ def get_bot_response():
     userText = request.args.get('msg')
     message = (bot.reply('localuser', userText))
     return  str(message)
-
-if __name__ == '__main__':
-   app.run()
